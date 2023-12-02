e71db2edcef51da9cff6a45b9416795a1fa1395f
David Rosario <david@lavacro.com>
2023-12-02 14:12:54 -0500

    One unit test working

--- src/test/java/com/lavacro/gym/dao/TestMuscleExerciseRepository.java
+++ src/test/java/com/lavacro/gym/dao/TestMuscleExerciseRepository.java
@@ -1,15 +1,23 @@
 package com.lavacro.gym.dao;
 
 import com.lavacro.gym.model.Exercise;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Example;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@AutoConfigureTestDatabase
+@ActiveProfiles(profiles = {"local"})
 public class TestMuscleExerciseRepository {
 	@Autowired
 	private MuscleExerciseRepository muscleExerciseRepository;
@@ -19,7 +27,15 @@ public class TestMuscleExerciseRepository {
 		muscleExerciseRepository.save(
 			new Exercise(1, "Abs", "Torso Rotation")
 		);
+		muscleExerciseRepository.save(
+				new Exercise(2, "Abs", "Hanging Knee Raise")
+		);
+		List<Exercise> exerciseList = muscleExerciseRepository.findAll();
+		Assertions.assertEquals(exerciseList.size(), 2);
+
 		Example<Exercise> example = Example.of(new Exercise(1, "Abs", "Torso Rotation"));
-		muscleExerciseRepository.findAll(example);
+		Optional<Exercise> optional = muscleExerciseRepository.findOne(example);
+
+		Assertions.assertTrue(optional.isPresent());
 	}
 }
